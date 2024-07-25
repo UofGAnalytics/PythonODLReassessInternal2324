b90e390049728114fb182bf2722b0fc67118a5c2
drraskell <56025951+drraskell@users.noreply.github.com>
2024-07-25 14:57:28 +0100

    Update __init__.py

--- reassesslib/__init__.py
+++ reassesslib/__init__.py
@@ -1,3 +1,9 @@
 from reassesslib.utils import random_draw
 
-__all__ = ["random_draw"]
+from reassesslib import load_data
+from reassesslib import movement_and_outliers
+from reassesslib import plotting
+
+__all__ = ["random_draw", "load_data", "movement_and_outliers", "plotting"]
+
+
